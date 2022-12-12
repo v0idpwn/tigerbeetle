52e8affd7d67c5ca8441f502a5e372ee8acfa7da
Phil Eaton <phil@eatonphil.com>
2022-12-12 13:30:06 -0500

    Start on client constructor

--- src/clients/go/docs.zig
+++ src/clients/go/docs.zig
@@ -27,14 +27,27 @@ pub const GoDocs = Docs{
     \\go mod init tbtest
     \\go mod tidy
     \\go build
-    ,
+            ,
 
+        
     .examples = 
     \\## Basic
     \\
     \\See [./samples/basic](./samples/basic) for a Go project
     \\showing many features of the client.
-    ,
+            ,
+
+        .client_object_example =
+        \\client, err := tb.NewClient(0, []string{"3000"}, 1)
+	\\if err != nil {
+	\\	log.Printf("Error creating client: %s", err)
+	\\	return
+	\\}
+	\\defer client.Close()"
+            ,
+
+        .client_object_documentation = "`NewClient` takes three arguments: a unique `uint32` representing the cluster ID, an array of string TCP addresses
+            for all  (`uint32`), addresses (`[]string`), and maxConcurrency uint"
 
     .developer_setup_bash_commands = 
     \\git clone https://github.com/tigerbeetledb/tigerbeetle
